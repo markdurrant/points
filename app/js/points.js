b972e5e37a95043788840465087cf0dc4b19bf64
Mark Durrant <mark@pebblecode.com>
2013-07-22 02:35:56 +0100

    add guide, and glyph styling

--- app/js/points.js
+++ app/js/points.js
@@ -1,19 +1,19 @@
-/*global $:true, console:true Path:true, Point:true, Color:true */
+/*global $:true, console:true Path:true, Point:true, Color:true, Group:true*/
 
 /*  GLYPH COORDINATES SYSTEM
 
        0   1   2
        _________
 
-0  |       A        A: [1,0]
+0  |       A        A(0): [1,0]
    |     / | \      
-1  |   B - C - D    B: [0,1]  C: [1,1]  D: [2,1]
+1  |   B - C - D    B(1): [0,1]   C(2): [1,1]   D(3): [2,1]
    |   | \ | / |    
-2  |   E - F - G    E: [0,2]  F: [1,2]  G: [2,2]
+2  |   E - F - G    E(4): [0,2]   F(5): [1,2]   G(6): [2,2]
    |   | / | \ |    
-3  |   H - I - J    H: [0,3]  I: [1,3]  J: [2,3]
+3  |   H - I - J    H(7): [0,3]   I(8): [1,3]   J(9): [2,3]
    |     \ | /      
-4  |       K        K: [1,4] */
+4  |       K        K(10):[1,4] */
 
 (function (){
 	'use strict';
@@ -21,19 +21,56 @@
 	// all glyph coordinates
 	var glyphPoints = [[1,0], [0,1], [1,1], [2,1], [0,2], [1,2], [2,2], [0,3], [1,3], [2,3], [1,4]];
 
-	// set scale of glyph points
-	var glyphScale = 100;
+	// set coordinate grid size 
+	var glyphScale = 60;
+
+	// set glyph starting point
+	var glyphStart = [60, 60];
+
+	// initalize glyph
+	var glyph = new Path();
+	
+	// set glyph styles
+	glyph.style = {
+		strokeColor: "000",
+    strokeWidth: 0.35 * glyphScale,
+    strokeCap: 'round',
+    strokeJoin: 'round'
+	};
 
 	// scale all glyph points
-	for (var i = glyphPoints.length - 1; i >= 0; i--) {
-		glyphPoints[i][0] *= glyphScale;
-		glyphPoints[i][1] *= glyphScale;
+	function scaleGlyph (glyphScale) {
+		for (var i = glyphPoints.length - 1; i >= 0; i--) {
+			glyphPoints[i][0] *= glyphScale;
+			glyphPoints[i][1] *= glyphScale;
+		}		
 	}
 
-	// var path = new Path();
-	// path.strokeColor = 'black';
+	// start glyph starting position
+	function setStart (glyphStartX, glyphStartY) {
+		for (var i = glyphPoints.length - 1; i >= 0; i--) {
+			glyphPoints[i][0] += glyphStartX;
+			glyphPoints[i][1] += glyphStartY;
+		}
+	}
 
-	// path.moveTo(glyphPoints[0]);
-	// path.lineTo(glyphPoints[1]);
+	// draw guides
+	function drawGuide () {
+		var path = new Path();
+		path.add(glyphPoints[0], glyphPoints[3], glyphPoints[7], glyphPoints[9], glyphPoints[1], glyphPoints[3], glyphPoints[9], glyphPoints[10], glyphPoints[7], glyphPoints[1], glyphPoints[0], glyphPoints[10]);
+
+		var path2 = new Path();
+		path2.add(glyphPoints[4], glyphPoints[6]);
+
+		var guides = new Group(path, path2);
+		guides.strokeColor = '5CF';
+		guides.sendToBack();
+	}
+
+	scaleGlyph(glyphScale);
+	setStart(glyphStart[0], glyphStart[1]);
+	drawGuide();
+
+	glyph.add(glyphPoints[4], glyphPoints[6]);
 
 })();
