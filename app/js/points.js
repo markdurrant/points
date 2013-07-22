3f2edb5b4a883a6e4cee7543a0184bf4c3e0309c
Mark Durrant <mark@pebblecode.com>
2013-07-22 01:41:32 +0100

    add scaling of glyph

--- app/js/points.js
+++ app/js/points.js
@@ -1,6 +1,6 @@
 /*global $:true, console:true Path:true, Point:true, Color:true */
 
-/*  COORDINATES SYSTEM
+/*  GLYPH COORDINATES SYSTEM
 
        0   1   2
        _________
@@ -18,5 +18,22 @@
 (function (){
 	'use strict';
 
+	// all glyph coordinates
+	var glyphPoints = [[1,0], [0,1], [1,1], [2,1], [0,2], [1,2], [2,2], [0,3], [1,3], [2,3], [1,4]];
+
+	// set scale of glyph points
+	var glyphScale = 100;
+
+	// scale all glyph points
+	for (var i = glyphPoints.length - 1; i >= 0; i--) {
+		glyphPoints[i][0] *= glyphScale;
+		glyphPoints[i][1] *= glyphScale;
+	}
+
+	// var path = new Path();
+	// path.strokeColor = 'black';
+
+	// path.moveTo(glyphPoints[0]);
+	// path.lineTo(glyphPoints[1]);
 
 })();
